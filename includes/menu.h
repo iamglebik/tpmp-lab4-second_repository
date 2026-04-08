b86f247bae031f9aac7e0fef87823970dfcf2a90
iamglebik <glebassinyakov@gmail.com>
2026-04-08 21:47:42 +0300

    Update menu.h

--- includes/menu.h
+++ includes/menu.h
@@ -3,7 +3,26 @@
 
 #include "database.h"
 
-void show_main_menu(void);
-int handle_menu_choice(Database *db, int choice);
+typedef enum {
+    ROLE_ADMIN = 0,
+    ROLE_DRIVER = 1
+} UserRole;
+
+typedef struct {
+    int id;
+    char username[50];
+    UserRole role;
+    int driver_id; 
+} Session;
+
+void show_main_menu(Session *session, Database *db);
+int handle_menu_choice(Session *session, Database *db, int choice);
+
+void show_admin_menu(void);
+void show_driver_menu(void);
+
+void input_car_form(Car *car);
+void input_driver_form(Driver *driver);
+void input_order_form(Order *order);
 
 #endif
